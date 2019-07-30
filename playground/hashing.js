8628b0b66232ada6ca0fedcce0726c5e13320c01
unknown <willyumburjer@gmail.com>
2019-07-30 21:17:16 +0800

    hash user assword efore hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,34 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-}
+// var data = {
+//   id: 10
+// }
 
-const token = jwt.sign(data, '123abc');
-console.log(token);
+// const token = jwt.sign(data, '123abc');
+// console.log(token);
 
-var decode = jwt.verify(token, '123abc');
-console.log(decode);
+// var decode = jwt.verify(token, '123abc');
+// console.log(decode);
 
 // const message = 'I am Ironman';
 // const hash = SHA256(message).toString();
 
 // console.log(`Message is ${message}`);
 // console.log(`the hashed is ${hash}`);
+
+//`````````````````````````````````````````````````````````````
+
+var pword = 'abc123?';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(pword, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+var hashedpw = '$2a$10$0DzFCRPOi6GDFmk.i6WmEe5q0PqCRpR7sehHdmOQiU8EuF/b9JUR2';
+
+bcrypt.compare(pword, hashedpw, (err, res) => {
+  console.log(res);
+});
