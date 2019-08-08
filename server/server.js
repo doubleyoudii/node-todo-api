84c88b15e4b6dd6ebefc13d667cf8ac62e40f894
unknown <willyumburjer@gmail.com>
2019-08-08 14:58:45 +0800

    add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -150,6 +150,26 @@ app.get('/users/me', authenticate, (req, res) => {
 });
 
 
+//`````````````post user login``````
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  // res.send({
+  //   email: body.email,
+  //   password: body.password
+  // });
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    // res.send(user);
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((err) => {
+    res.status(400).send();
+  })
+})
+
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
